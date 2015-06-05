4cbe02c3f46ec1afe260f1f7c42744735c318632
Santi Castells <santi.castells@fancybox.com>
2015-06-05 13:41:15 +0200

    bundle structure

---
+++ ConektaBundle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scastells
+ * Date: 5/06/15
+ * Time: 11:45
+ */
+namespace Fancy\ConektaBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ConektaBundle extends Bundle
+{
+}
---
+++ Controller/ConektaController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: scastells
+ * Date: 5/06/15
+ * Time: 11:52
+ */
+
+namespace Fancy\ConektaBundle;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ConektaController extends Controller
+{
+
+}
